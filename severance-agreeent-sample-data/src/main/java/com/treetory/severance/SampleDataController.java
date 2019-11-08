722f6a8c3fe4d195f518e4ebd61b3f7f3379afca
treetory <treetory@gmail.com>
2019-11-08 15:48:22 +0900

    오타 수정

--- severance-agreeent-sample-data/src/main/java/com/treetory/severance/SampleDataController.java
+++ severance-agreeent-sample-data/src/main/java/com/treetory/severance/SampleDataController.java
@@ -16,8 +16,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @RestController
