da76313f65ec2d6966db3d62a4fe3fe68f977c83
treetory <treetory@gmail.com>
2019-12-04 14:12:17 +0900

    수식 테스트

--- severance-agreeent-sample-data/src/main/resources/static/js/function_call.js
+++ severance-agreeent-sample-data/src/main/resources/static/js/function_call.js
@@ -15,12 +15,23 @@ let data = {
 let fn1 =new Function("return 1 + 2");
 console.log(fn1());
 
-// 2. function string 으로 되어 있는 것을 바로 실행하는 방법
-eval("var fn2 = function() { return 1 + 2; }");
+let fn2 =new Function("{ var a = 5; var b = 3; return a + b }");
 console.log(fn2());
 
-// 3. object 내부에 value 로 기술되어 있는 function string 을 실행할 수 있게 만들고, 실행 처리하는 방법
-eval("var fn3 = " + data["CalculateInfo"]);
+// 2. Math 의 연산 메서드 사용하는 방법
+let fn3 =new Function("{ return Math.abs(15.235) + Math.floor(20.1) }");
 console.log(fn3());
+
+let fn4 =new Function("{ return Math.PI }");
+console.log(fn4());
+
+// 3. function string 으로 되어 있는 것을 바로 실행하는 방법
+eval("var fn11 = function() { return 1 + 2; }");
+console.log(fn11());
+
+// 4. object 내부에 value 로 기술되어 있는 function string 을 실행할 수 있게 만들고, 실행 처리하는 방법
+eval("var fn12 = " + data["CalculateInfo"]);
+console.log(fn12());
+
 
 // 주의사항 : eval 내부에선 var 로 선언된 변수만 받는다. let, const 는 지원되지 않음.
